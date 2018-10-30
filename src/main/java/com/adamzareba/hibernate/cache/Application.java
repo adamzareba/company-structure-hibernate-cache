43849f40e2566ebcfbbb788c33941d0cb1c5af1b
Adam Zaręba <bontade88@gmail.com>
2018-10-30 13:12:05 +0100

    upgraded com.fasterxml.jackson.core:jackson-databind to version 2.9.5

--- src/main/java/com/adamzareba/hibernate/cache/Application.java
+++ src/main/java/com/adamzareba/hibernate/cache/Application.java
@@ -7,14 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 import org.springframework.context.annotation.EnableLoadTimeWeaving;
 
-import static org.springframework.context.annotation.EnableLoadTimeWeaving.AspectJWeaving;
-
 @SpringBootApplication(exclude = JmxAutoConfiguration.class)
-@EnableLoadTimeWeaving(aspectjWeaving = AspectJWeaving.ENABLED)
+@EnableLoadTimeWeaving
 public class Application {
 
     public static void main(String[] args) {
         DynamicInstrumentationLoader.waitForInitialized(); //dynamically attach java agent to jvm if not already present
+        DynamicInstrumentationLoader.initLoadTimeWeavingContext(); //weave all classes before they are loaded as beans
         SpringApplication.run(Application.class, args);
     }
 }
